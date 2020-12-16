8e4d2a5cac5dcd1ef317e2ab8efb04cbc4b2f374
Duy Truong <duytruongh@gmail.com>
2020-12-15 21:05:54 -0800

    15122020 - Updated HTML formatted, added JavaScript file

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch(url) // Call the fetch function passing url of API
+.then(function(){
+    // Code for handling data from API
+})
+.catch(function(){
+    //Run code if server returns any errors
+});
