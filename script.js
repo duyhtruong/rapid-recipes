80f6dbe68f159e1a7a24be86f4b79a2a40c22f05
Duy Truong <duytruongh@gmail.com>
2020-12-16 20:34:08 -0800

    16122020 - Call API on click functionality added

--- script.js
+++ script.js
@@ -9,27 +9,30 @@ function append(parent, el){
     return parent.appendChild(el); // append second parameter to first
 }
 
-fetch(url,{
-    mode: 'cors',
-    headers:{
-        'Content-Type': 'application.json'
-    }
-}) // Call the fetch function passing url of API
-.then((resp)=>resp.json()) // Transform the data into json
-.then(function(data){
-    let authors = data.results; // get the results
-    return authors.map(function(author){ //map through results
-        let li = createNode('li'), // Create elements we need
-            img = createNode('img'),
-            span = createNode('span');
-        img.src = author.picture.medium; // Add source of image to be src of img
-        span.innerHTML = `${author.name.first} ${author.name.last}`; //add author first and last name in span
-        append(li, img);
-        append(li, span);
-        append(ul,li);
+function searchAPI(){
+    fetch(url,{
+        mode: 'cors',
+        headers:{
+            'Content-Type': 'application.json'
+        }
+    }) // Call the fetch function passing url of API
+    .then((resp)=>resp.json()) // Transform the data into json
+    .then(function(data){
+        let authors = data.results; // get the results
+        return authors.map(function(author){ //map through results
+            let li = createNode('li'), // Create elements we need
+                img = createNode('img'),
+                span = createNode('span');
+            img.src = author.picture.medium; // Add source of image to be src of img
+            span.innerHTML = `${author.name.first} ${author.name.last}`; //add author first and last name in span
+            append(li, img);
+            append(li, span);
+            append(ul,li);
+        })
     })
-})
-.catch(function(error){
-    console.log(error);
-    //Run code if server returns any errors
-});
+    .catch(function(error){
+        console.log(error);
+        //Run code if server returns any errors
+    });
+}
+
