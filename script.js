5f6b27a558ce991d32d9d07c235ed320a7c06ed6
Duy Truong <duytruongh@gmail.com>
2020-12-17 21:16:18 -0800

    17122020 - Card outlines set

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const ul = document.getElementById('resultsList');
-const url = 'https://randomuser.me/api/?results=10';
+const url = 'https://randomuser.me/api/?results=3';
 
 function createNode(element){
     return document.createElement(element); // Create type of element you pass in param
@@ -22,10 +22,23 @@ function searchAPI(){
         return authors.map(function(author){ //map through results
             let li = createNode('li'), // Create elements we need
                 img = createNode('img'),
-                span = createNode('span');
-            img.src = author.picture.medium; // Add source of image to be src of img
-            span.innerHTML = `${author.name.first} ${author.name.last}`; //add author first and last name in span
+                span = createNode('span'),
+                authorFirstName = createNode('p'),
+                authorLastName = createNode('p'),
+                authorEmail = createNode('p'),
+                authorAge = createNode('p');
 
+            img.src = author.picture.large; // Add source of image to be src of img
+            authorFirstName.innerHTML=`${author.name.first}`;
+            authorLastName.innerHTML=`${author.name.last}`;
+            authorEmail.innerHTML=`${author.email}`;
+            authorAge.innerHTML=`${author.dob.age}`;
+
+            span.classList.add('resultTextGroup')
+            append(span,authorFirstName);
+            append(span,authorLastName);
+            append(span,authorEmail);
+            append(span,authorAge);
             append(li, img);
             append(li, span);
             li.classList.add("resultStyle");
